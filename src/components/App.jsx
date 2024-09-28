2d47a5a3720f9f1d329db0023650a464183a860e
StefaniaGuta <142820560+StefaniaGuta@users.noreply.github.com>
2024-09-28 06:40:28 +0300

    SideBar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from '../pages/HomePage/HomePage';
 import LoginPage from '../pages/LogInPage/LoginPage';
 import RegistrationPage from '../pages/RegistrationPage/RegistrationPage';
+import SideBar from './SideBar/SideBar';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegistrationPage />} />
+        <Route path='/sidebar' element={<SideBar />} />
       </Routes>
     </Router>
   );
