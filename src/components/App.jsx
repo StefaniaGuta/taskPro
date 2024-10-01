7a79b69aee3088b510bdfe58cb6edaa0608485de
StefaniaGuta <142820560+StefaniaGuta@users.noreply.github.com>
2024-10-01 21:56:08 +0300

    Revert "Projectpage"

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,17 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from '../pages/HomePage/HomePage';
-import LoginPage from '../pages/LogInPage/LoginPage';
-import RegistrationPage from '../pages/RegistrationPage/RegistrationPage';
-import SideBar from './SideBar/SideBar';
-import ProjectOffice from './ProjectOffice/ProjectOffice';
-
-const App = () => {
+export const App = () => {
   return (
-    <Router basename="/taskPro">
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegistrationPage />} />
-        <Route path="/sidebar" element={<SideBar />} />
-        <Route path="/projectoffice" element={<ProjectOffice />} />
-      </Routes>
-    </Router>
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+        
+      }}
+    >
+      React homework template
+    </div>
   );
 };
-
-export default App;
