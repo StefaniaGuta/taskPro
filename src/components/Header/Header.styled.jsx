d18181a55487b237a0b237f73cbb55528c91d1ba
StefaniaGuta <142820560+StefaniaGuta@users.noreply.github.com>
2024-10-09 15:45:06 +0300

    theme

--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -7,27 +7,7 @@ export const HeaderSection = styled.header`
   height: 40px;
   padding: 10px;
   position: relative;
-  background-color: ${({ theme }) => {
-    switch (theme) {
-      case 'dark':
-        return '#161616';
-      case 'violet':
-        return '#ffffff';
-      default:
-        return '#ffffff';
-    }
-  }};  
-  color: ${({theme}) =>{
-    switch (theme) {
-      case 'dark':
-        return '#FFFFFF';
-      case 'violet':
-        return '#161616';
-      default:
-        return '#161616';
-    }
-  }}
-  
+  background-color: #ffff;
 `;
 
 
